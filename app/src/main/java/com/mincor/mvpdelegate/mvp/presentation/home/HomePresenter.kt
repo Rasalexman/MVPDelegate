306b64e75f636125a60deafffab0e02e323b7890
Alexander <sphc@yandex.ru>
2019-07-19 02:23:22 +0300

    added coroutines manager

--- app/src/main/java/com/mincor/mvpdelegate/mvp/presentation/home/HomePresenter.kt
+++ app/src/main/java/com/mincor/mvpdelegate/mvp/presentation/home/HomePresenter.kt
@@ -1,8 +1,14 @@
 package com.mincor.mvpdelegate.mvp.presentation.home
 
 import com.mincor.mvpdelegate.mvp.base.lifecycle.BaseLifecyclePresenter
+import com.rasalexman.coroutinesmanager.CoroutinesProvider
+import com.rasalexman.coroutinesmanager.ICoroutinesManager
+import com.rasalexman.coroutinesmanager.launchOnUITryCatchFinally
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 
-class HomePresenter : BaseLifecyclePresenter<IHomeContract.IView>(), IHomeContract.IPresenter {
+class HomePresenter : BaseLifecyclePresenter<IHomeContract.IView>(),
+    IHomeContract.IPresenter, ICoroutinesManager {
 
     override fun onViewAttached(view: IHomeContract.IView) {
         super.onViewAttached(view)
@@ -10,14 +16,28 @@ class HomePresenter : BaseLifecyclePresenter<IHomeContract.IView>(), IHomeContra
         println("VIEW ISA ATTACHED")
     }
 
-    override fun sayHello() {
-        /*
-        view?.stickySuspension<Unit> {
-            showHello(TAG)
-        }*/
+    override fun sayHello() = launchOnUITryCatchFinally(
+        tryBlock = {
 
-        view?.showHello(TAG)
-    }
+            val sum = withContext(CoroutinesProvider.COMMON) {
+                delay(10000L)
+
+                var period = 0f
+                for (i in 0 until 100) {
+                    period += i * 0.45f
+                }
+
+                period
+            }
+
+            view().stickySuspension {
+                showHello("$TAG period = $sum")
+            }
+        }, catchBlock = {
+
+        }, finallyBlock = {
+
+        })
 
     companion object {
         const val TAG = "HomePresenter"
